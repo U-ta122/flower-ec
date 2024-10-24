4c275e7754085c088e73a86d19f558e8d9ba31cc
U-ta122 <yuta8038@gmail.com>
2024-10-24 19:06:25 +0900

    deploy

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -2,7 +2,7 @@
     <form action='/shop/post/create' method='POST'>
         @csrf
         <p>告知文：</p>
-        <textarea name="post[text]" placeholder="例：薔薇のアレンジメントを作成しました"></textarea><br>
+        <textarea name="post[text]" placeholder="例：薔薇のアレンジメントです"></textarea><br>
         <input type="submit"></input>
     </form>
     
